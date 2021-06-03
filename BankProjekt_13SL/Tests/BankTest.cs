b74ce4b0c26327cde99cc7e73a8939cd36fa4c5e
Budaházi Bence <budahazi.bence@petrik.hu>
2021-06-03 17:32:37 +0200

    EgyenlegFeltolt teszt

--- BankProjekt_13SL/Tests/BankTest.cs
+++ BankProjekt_13SL/Tests/BankTest.cs
@@ -20,6 +20,11 @@ namespace BankProjekt_13SL.Tests
         {
             b.UjSzamla("Teszt Elek", "1234");
         }
+
+        private void NagyArpad5678()
+        {
+            b.UjSzamla("Nagy Árpád", "5678");
+        }
         [TestCase]
         public void UjSzamlaSikeresenLetrejon()
         {
@@ -55,6 +60,43 @@ namespace BankProjekt_13SL.Tests
             TesztElek1234();
             Assert.Throws<HibasSzamlaszamException>(() =>
                 b.Egyenleg("4321"));
+        }
+
+        [TestCase]
+        public void EgyenlegFeltoltNemLetezoSzamlaraExceptiontDob()
+        {
+            TesztElek1234();
+            Assert.Throws<HibasSzamlaszamException>(() =>
+                b.EgyenlegFeltolt("4321", 10000));
+        }
+        [TestCase]
+        public void EgyenlegFeltoltSikeresenMegvaltozikAzEgyenleg()
+        {
+            TesztElek1234();
+            Assert.AreEqual(0, b.Egyenleg("1234"));
+            b.EgyenlegFeltolt("1234", 10000);
+            Assert.AreEqual(10000, b.Egyenleg("1234"));
+        }
+        [TestCase]
+        public void EgyenlegFeltoltNullaOsszeg()
+        {
+            TesztElek1234();
+            Assert.Throws<ArgumentException>(() =>
+                b.EgyenlegFeltolt("1234", 0));
+        }
+        [TestCase]
+        public void EgyenlegFeltoltAMegfeleloSzamlaraTolt()
+        {
+            TesztElek1234();
+            NagyArpad5678();
+            Assert.AreEqual(0, b.Egyenleg("1234"));
+            Assert.AreEqual(0, b.Egyenleg("5678"));
+
+            b.EgyenlegFeltolt("1234", 10000);
+
+            Assert.AreEqual(10000, b.Egyenleg("1234"));
+            Assert.AreEqual(0, b.Egyenleg("5678"));
+
         }
     }
 }
